bfff190f69a8d1d09c729e2f7550c07d86b57956
Anna <kochurovaav@gmail.com>
2023-02-03 14:01:50 +0200

    Task002 is done

---
+++ Task002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа по заданному номеру четверти выдает диапазон возможных координат точек в этой четверти (X,y)
+
+System.Console.WriteLine("Enter section: ");
+
+int Section = int.Parse(Console.ReadLine());
+
+if (Section == 1)
+{
+    Console.WriteLine("X>0, Y>0");
+}
+else 
+if (Section == 2)
+{
+    Console.WriteLine("X<0, Y>0");
+}
+else 
+if (Section == 3)
+{
+    Console.WriteLine("X<0, Y<0");
+}
+else 
+if (Section == 4)
+{
+    Console.WriteLine("X<>0, Y<0");
+}
