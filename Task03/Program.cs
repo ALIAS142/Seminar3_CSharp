f1a9b282b3f67a055d4ab8b78ce6ccfa09d4552a
Anna <kochurovaav@gmail.com>
2023-02-03 16:46:55 +0200

    Task03 is done

---
+++ Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа принимает координаты двух точек и находит расстояние междк ними в 2D -плоскости.
+
+System.Console.Write("Enter Point's A coordinats X:  ");
+double AX = double.Parse(Console.ReadLine());
+System.Console.Write("Enter Point's A coordinats Y:  ");
+double AY = double.Parse(Console.ReadLine());
+
+
+System.Console.Write("Enter Point's B coordinats X:  ");
+
+double BX = double.Parse(Console.ReadLine());
+System.Console.Write("Enter Point's B coordinats Y:  ");
+double BY = double.Parse(Console.ReadLine());
+
+double C1 = AY - BY;
+double C2 = AX - BX;
+
+double result = (C1 * C1) + (C2 * C2);
+double result2 = Math.Sqrt(result);
+
+System.Console.WriteLine(result2);
