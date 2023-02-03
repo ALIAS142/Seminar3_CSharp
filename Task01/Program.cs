1191cda518124d5fbdf060337172a32c2d6c90ee
Anna <kochurovaav@gmail.com>
2023-02-03 13:30:54 +0200

    First task is added

---
+++ Task01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа принимает на вход координаты точки (X, Y), где
+// X != 0 И Y !=0. На выходе: номер четвери плоскости,  в которой находится эта точка.
+
+System.Console.Write("Enter number X: ");
+int numX = int.Parse(Console.ReadLine());
+
+System.Console.Write("Enter number Y: ");
+int numY = int.Parse(Console.ReadLine());
+
+if (numX > 0 & numY > 0)
+{
+    Console.WriteLine("1 section");
+}
+else 
+if (numX < 0 & numY > 0)
+{
+    Console.WriteLine("2 section");
+}
+else 
+if (numX < 0 & numY < 0)
+{
+    Console.WriteLine("3 section");
+}
+else 
+if (numX > 0 & numY < 0)
+{
+    Console.WriteLine("4 section");
+}
