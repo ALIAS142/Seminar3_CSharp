60a872f156fe19e44e9b5256af812252e56f84df
Anna <kochurovaav@gmail.com>
2023-02-05 11:46:07 +0200

    Task04 is done

---
+++ Task04/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает число N и выдает таблицу квадратов этого числа.
+
+System.Console.WriteLine("Enter number:  ");
+int numN = int.Parse(Console.ReadLine());
+
+int numK = 1;
+
+while (numK <= numN)
+{
+     System.Console.Write(Convert.ToInt32(Math.Pow(numK, 2)));
+    
+     numK = numK + 1;
+}
+  
+
+
+
+
+
+
